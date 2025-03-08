968ff9b1dbe30c83dc025c56ae45a60624ff74c7
Mikki Monroe <mikkimonroexo@gmail.com>
2025-03-07 23:40:30 -0600

    update core/start.sh to set docker.dock permissions

--- recoverysky-core/start.sh
+++ recoverysky-core/start.sh
@@ -2,4 +2,7 @@
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 306057522130.dkr.ecr.us-east-1.amazonaws.com
 
+sudo chown root:docker /var/run/docker.sock
+sudo chmod 0660 /var/run/docker.sock
+
 docker-compose up -d
