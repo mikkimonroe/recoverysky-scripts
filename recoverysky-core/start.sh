14c0c977d15891c052691ccfa6af7cd75db77a8a
Mikki Monroe <mikkimonroexo@gmail.com>
2025-03-07 23:58:07 -0600

    debugging

--- recoverysky-core/start.sh
+++ recoverysky-core/start.sh
@@ -5,4 +5,6 @@ aws ecr get-login-password --region us-east-1 | docker login --username AWS --pa
 sudo chown root:docker /var/run/docker.sock
 sudo chmod 0660 /var/run/docker.sock
 
+echo $(eval 'ls -la /var/run/docker.sock ')
+
 docker-compose up -d
